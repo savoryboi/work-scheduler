4029ae312cb462479b3d6be36d4675e6df3f84f3
savoryboi <andbjerk@yahoo.com>
2022-07-05 23:33:39 -0500

    working color coder

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,14 @@
 var currentDayEl = document.querySelector('#currentDay')
 currentDayEl.innerText = moment().format('LL');
-var blockTimeArray = [8, 9, 10, 11, 12, 13, 14, 15, 16];
-var timeBlockEls = document.querySelectorAll('.time-block');
-var currentHour = moment().hour();
+var timeBlockArray = [8, 9, 10, 11, 12, 13, 14, 15, 16];
+var currentHour = 13;
+var timeBlockEls = document.querySelectorAll('.time-block')
+var hourBlockArray = [hour8, hour9, hour10, hour11, hour12, hour13, hour14, hour15, hour16];
 
+// grab each timeblock element for future reference
 var hour8 = document.querySelector('#hour8');
 var hour9 = document.querySelector('#hour9');
-var hour13 = document.querySelector('#hour10');
+var hour10 = document.querySelector('#hour10');
 var hour11 = document.querySelector('#hour11');
 var hour12 = document.querySelector('#hour12');
 var hour13 = document.querySelector('#hour13');
@@ -14,17 +16,20 @@ var hour14 = document.querySelector('#hour14');
 var hour15 = document.querySelector('#hour15');
 var hour16 = document.querySelector('#hour16');
 
-console.log(currentHour)
+
 function setTimeBlock() {
-
-}  
-
-
-
-
-
-
-function colorCode() {
+   
+    timeBlockEls.forEach((item)=>{
+        if(item.dataset.hour < currentHour){
+            item.classList.add('past');
+        } else if(item.dataset.hour == currentHour){
+            item.classList.add('present');
+        } else if(item.dataset.hour > currentHour){
+            item.classList.add('future');
+        }
+        
+    })  
 
 }
+
 setTimeBlock();
